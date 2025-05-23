2de4c47506011264df24efed9450c16285867c50
Prasanna Achar <prasannaachar64@gmail.com>
2025-05-23 11:21:47 +0530

    first Commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import 'dotenv/config';
+import cors from 'cors'
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from './controllers/clerkWebhooks.js';
+
+
+connectDB()
+
+
+const app = express()
+app.use(cors()) //enable croos-=origin resource sharing
+
+// middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+
+// api to listin clerk webhook
+app.use("/api/clerk", clerkWebhooks);
+
+
+app.get('/',(req, res)=> res.send("API IS WORKING"))
+
+const PORT = process.env.PORT  || 3000;
+
+app.listen(PORT, ()=> console.log(`Server runing on port ${PORT}`));
+
